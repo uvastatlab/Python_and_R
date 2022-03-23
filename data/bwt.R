f445a3a9e86da4f7bd069ac3f9dc3d43523931d2
Clay Ford <joe.clayton.ford@gmail.com>
2022-03-23 16:49:00 -0400

    add logistic regression data and R section

--- data/bwt.R
+++ data/bwt.R
@@ -2,13 +2,15 @@
 # Risk Factors Associated with Low Infant Birth Weight
 library(MASS)
 # ?birthwt
+data("birthwt")
 bwt <- with(birthwt, {
   race <- factor(race, labels = c("white", "black", "other"))
-  ptd <- factor(ptl > 0)
+  ptd <- factor(ifelse(ptl > 0, 1, 0))
   ftv <- factor(ftv)
   levels(ftv)[-(1:2)] <- "2+"
-  data.frame(low = factor(low), age, lwt, race, smoke = (smoke > 0),
-             ptd, ht = (ht > 0), ui = (ui > 0), ftv)
+  data.frame(low = low, age, lwt, race, 
+             smoke = factor(smoke),
+             ptd, ht = factor(ht), ui = factor(ui), ftv)
 })
-write.csv(bwt, file = "bwt.csv", row.names = FALSE)
+# write.csv(bwt, file = "bwt.csv", row.names = FALSE)
 saveRDS(bwt, file = "bwt.Rds")
